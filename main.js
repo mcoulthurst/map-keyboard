9322f49e7d40781168c834779769db16365daa14
Martin Coulthurst <mc@lightness.co.uk>
2023-09-01 12:50:37 +0100

    Tidy up

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const defaultStyle = new Style({
     color: 'rgba(255, 255, 255, 0.1)',
   }),
   stroke: new Stroke({
-    color: 'rgba(0, 0, 0, 0.1)',
+    color: 'rgba(0, 0, 0, 0.4)',
     width: 0.5,
   }),
 });
