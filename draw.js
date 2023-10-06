5a450ab1f74f5b8c3929d879a88a6a3b7c479cb3
Martin Coulthurst <mc@lightness.co.uk>
2023-10-06 15:20:00 +0100

    style the lineString (work-in-progress)

--- draw.js
+++ draw.js
@@ -1,15 +1,52 @@
 import './style.css';
 import { Map, View } from 'ol';
+import Point from 'ol/geom/Point.js';
 import Draw from 'ol/interaction/Draw.js';
 import OSM from 'ol/source/OSM';
 import { Vector as VectorSource } from 'ol/source.js';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
-
+import {Fill, RegularShape, Stroke, Style} from 'ol/style.js';
 
 let firstPoint = [];
 let center = [];
 let featureArray = [];
 let draw; // global so we can remove it later
+
+
+const styleFunction = function (feature) {
+  const geometry = feature.getGeometry();
+  const styles = [
+    // linestring
+    new Style({
+      stroke: new Stroke({
+        color: '#0000FF',
+        width: 4,
+        dash: [10, 10],
+      }),
+    }),
+  ];
+/* 
+  geometry.forEachSegment(function (start, end) {
+    const dx = end[0] - start[0];
+    const dy = end[1] - start[1];
+    const rotation = Math.atan2(dy, dx);
+    // arrows
+    styles.push(
+      new Style({
+        geometry: new Point(end),
+//         image: new Icon({
+//          src: 'data/arrow.png',
+//          anchor: [0.75, 0.5],
+//          rotateWithView: true,
+//          rotation: -rotation,
+//        }), 
+      })
+    );
+  });
+ */
+  return styles;
+};
+
 
 const vectorSource = new VectorSource({
   features: featureArray,
@@ -17,6 +54,7 @@ const vectorSource = new VectorSource({
 
 const vectorLayer = new VectorLayer({
   source: vectorSource,
+  style: styleFunction,
 });
 
 const tileLayer = new TileLayer({
@@ -33,18 +71,77 @@ const map = new Map({
   }),
 });
 
+/* 
+//  custom points: w-i-p
+const stroke = new Stroke({color: 'black', width: 2});
+const fill = new Fill({color: 'red'});
+var pointStyle = new Style({
+    image: new RegularShape({
+      fill: fill,
+      stroke: stroke,
+      points: 4,
+      radius: 10,
+      angle: Math.PI / 4,
+    }),
+  });
+ */
+
 
 function addInteraction() {
   draw = new Draw({
     source: vectorSource,
     features: featureArray,
     type: "LineString",
+    /* style: pointStyle, */
   });
   map.addInteraction(draw);
 }
 
 addInteraction();
 
+var drawStyle = function(feature) {
+  var styles = [];
+  if (feature.getGeometry().getType() == 'LineString') {
+      var coordinates = feature.getGeometry().getCoordinates();
+      styles.push(new ol.style.Style({
+          geometry: new ol.geom.LineString(coordinates.slice(-2)),
+          stroke: new ol.style.Stroke({
+              color: '#0000FF',
+              lineDash: [1, 20],
+              width: 5,
+            lineCap:'square',
+            lineJoin:'round',
+            lineDashOffset:10
+          })
+      })); 
+      styles.push(new ol.style.Style({
+          geometry: new ol.geom.MultiPoint(coordinates),
+          image: new ol.style.RegularShape({
+            points: 4,
+            radius: 6,
+            angle: Math.PI / 4,
+            fill: new ol.style.Fill({
+              color: 'blue'
+            }),
+            stroke: new ol.style.Stroke({
+              color: 'blue'
+            }),
+          })
+      })); 
+      if (coordinates.length > 2) {
+        styles.push(new ol.style.Style({
+          geometry: new ol.geom.LineString(coordinates.slice(0,-1)),
+          stroke: new ol.style.Stroke({
+              color: '#0000FF',
+              width: 2,
+            lineCap:'square',
+            lineJoin:'round'
+          })
+        }));
+      }
+  }
+  return styles;
+}
 
 map.on('click', function (evt) {
   let coordinate = evt.coordinate;
@@ -74,21 +171,39 @@ function addMarker(center) {
 function closeLoop() {
   console.log("closeLoop");
   draw.appendCoordinates(firstPoint);
+
   draw.finishDrawing();
+  // reset the first point
+  firstPoint = [];
+
   // Get the array of features
   let features = vectorSource.getFeatures()
-  console.log(vectorSource);
-
   features = vectorLayer.getSource().getFeatures();
-  console.log(vectorLayer);
-  console.log(features);
+  
+  //console.log(vectorSource);
+  //console.log(vectorLayer);
+  //console.log(features);
+
 
   // Go through this array and get coordinates of their geometry.
   features.forEach(function (feature) {
-    console.log(feature.getGeometry().getCoordinates());
+    var coords = feature.getGeometry().getCoordinates();
+    let str = ""
+    coords.forEach(function (point) {
+      str += "[" + point + "],<br />";
+    });
+    traceOut(str)
   });
 }
 
+
+
+function traceOut(txt) {
+  console.log(txt);
+  var paragraph = document.getElementById("trace");
+  var existing = paragraph.innerHTML;
+  paragraph.innerHTML = existing + "<br/>" + txt;
+}
 
 
 function clearAllMarkers() {
